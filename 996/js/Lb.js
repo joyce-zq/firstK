a06b60b9fe17b6d5c056ed7397063375db2fec31
joyce-zq <zq18780264990>
2020-05-23 11:18:39 +0800

    提交文件

---
+++ 996/js/Lb.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Administrator on 2020/5/18 0018.
+ */
+"use strict";
+// var imgArr=["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg"];
+// var id;
+// var imgNum=imgArr.length;
+// // console.log(imgNum);
+// $("figure").click(function () {
+//     $("#lightbox").hide();
+//     $("#zhezhao").show();
+//     id=$(this).attr("data-id");
+//     var num=id;
+//     $("#bigimg").attr("src",imgArr[id]);
+//     $(".toNext").click(function () {
+//         num++;
+//         if(num>(imgNum-1)){
+//             num=0;
+//         }
+//         $("#bigimg").attr("src",imgArr[num]);
+//     });
+//     $(".toPre").click(function () {
+//         num--;
+//         if(num<0){
+//             num=(imgNum-1);
+//         }
+//         $("#bigimg").attr("src",imgArr[num]);
+//     })
+//
+// });
+// $(".close").click(function () {
+//     $("#zhezhao").hide();
+//     $("#lightbox").show();
+// });
+(function () {
+    var lightBox={
+        init:function () {
+            
+        },
+        addZhezhao:function () {
+            
+        }
+    };
+})();
